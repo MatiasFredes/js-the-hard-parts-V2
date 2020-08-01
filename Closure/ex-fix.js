a4f10c6dab0af5d871e7b38731145779c65ff5ee
Matias <mnfredes@gmail.com>
2020-08-01 14:21:16 -0300

    Solve exercise 3, 4, 5

--- Closure/ex-fix.js
+++ Closure/ex-fix.js
@@ -41,56 +41,66 @@ const jasCounter = outer();
 // Before your do, guess what will be logged from each function call.
 
 // /*** Uncomment these to check your work! ***/
-// willCounter();
-// willCounter();
-// willCounter();
+willCounter();
+willCounter();
+willCounter();
 
-// jasCounter();
-// willCounter();
+jasCounter();
+willCounter();
 
 
 function addByX(x) {
-
+  return y => x + y;
 }
 
 // /*** Uncomment these to check your work! ***/
-// const addByTwo = addByX(2);
-// addByTwo(1); // => should return 3
-// addByTwo(2); // => should return 4
-// addByTwo(3); // => should return 5
+const addByTwo = addByX(2);
+addByTwo(1); // => should return 3
+addByTwo(2); // => should return 4
+addByTwo(3); // => should return 5
 
-// const addByThree = addByX(3);
-// addByThree(1); // => should return 4
-// addByThree(2); // => should return 5
+const addByThree = addByX(3);
+addByThree(1); // => should return 4
+addByThree(2); // => should return 5
 
-// const addByFour = addByX(4);
-// addByFour(4); // => should return 8
-// addByFour(5); // => should return 9
+const addByFour = addByX(4);
+addByFour(4); // => should return 8
+addByFour(5); // => should return 9
 
 
 // CHALLENGE 4
 function once(func) {
-
+  let result;
+  return num => {
+    if(result === undefined)
+      result = func(num);
+    return result;
+  }
 }
 
 // /*** Uncomment these to check your work! ***/
-// const onceFunc = once(addByTwo);
-// console.log(onceFunc(4));  // => should log 6
-// console.log(onceFunc(10));  // => should log 6
-// console.log(onceFunc(9001));  // => should log 6
+const onceFunc = once(addByTwo);
+console.log(onceFunc(4));  // => should log 6
+console.log(onceFunc(10));  // => should log 6
+console.log(onceFunc(9001));  // => should log 6
 
 
 // CHALLENGE 5
 function after(count, func) {
-
+  let numberOfCalls = 0;
+  return () => {
+    numberOfCalls++;
+    if(numberOfCalls === count)
+      func();
+  }
 }
 
 // /*** Uncomment these to check your work! ***/
-// const called = function() { console.log('hello') };
-// const afterCalled = after(3, called);
-// afterCalled(); // => nothing is printed
-// afterCalled(); // => nothing is printed
-// afterCalled(); // => 'hello' is printed
+const called = function() { console.log('hello') };
+const afterCalled = after(3, called);
+afterCalled(); // => nothing is printed
+afterCalled(); // => nothing is printed
+afterCalled(); // => 'hello' is printed
 
 
 // CHALLENGE 6
